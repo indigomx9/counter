853b77f67be773040ae5c5c4ac6850d638e938cf
indigomx9 <indigomx9@gmail.com>
2018-06-10 19:46:13 -0400

    Setup of the Counter app.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './App.css';
+
+let App = () => {
+    return (
+        <div>
+            <h1 className={classes.banner}>Counter</h1>
+            <h1>Welcome to the Counter!</h1>
+        </div>
+    );
+};
+
+export default App;
+
